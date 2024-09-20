cf219f8bbe9ce219b7e17ba6f36a8229f8caaf6a
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-09-20 11:15:39 -0300

    feat: add layout for clients page and layout for table

---
+++ emanuel-project/src/scripts/clients.js
@@ -0,0 +1,20 @@
+const renderDataListInput = () => {
+  const input = document.querySelector('#client-input')
+  const datalist = document.querySelector('#clients-list')
+
+  if (input.value !== '') {
+    users.filter(user => user.name.includes(input.value)).forEach(user => {
+      const option = document.createElement('option')
+      option.value = user.name
+      datalist.appendChild(option)
+    })
+  } else {
+    users.sort().forEach(user => {
+      const option = document.createElement('option')
+      option.value = user.name
+      datalist.appendChild(option)
+    })
+  }
+}
+
+renderDataListInput()
