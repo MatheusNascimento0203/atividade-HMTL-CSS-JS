7f39cd0a77012bc020d269a2b5be572ab2a801a5
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-09-20 13:00:49 -0300

    feat: add logic for render table

--- emanuel-project/src/scripts/clients.js
+++ emanuel-project/src/scripts/clients.js
@@ -18,3 +18,86 @@ const renderDataListInput = () => {
 }
 
 renderDataListInput()
+
+const renderClients = () => {
+  const clientsTable = document.querySelector('#clients-table')
+
+  clientsTable.innerHTML = ''
+
+  users.forEach(user => {
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+      <td class="container-name">
+        <div class="avatar-table">
+          <span>${user.initials}</span>
+        </div>
+
+        <div class="container-name-and-email">
+          <span class="name-table">${user.name}</span>
+          <span class="email-table">${user.email}</span>
+        </div>
+      </td>
+      <td>
+        <span>${user.phone}</span>
+      </td>
+      <td>
+        <span>${user.plan}</span>
+      </td>
+      <td>
+        <span class=${user.status === true ? 'status-active' : 'status-inactive'}>${user.status === true ? 'Ativo' : 'Inativo'}</span>
+      </td>
+      <td>
+        <span>10</span>
+      </td>
+      <td class="container-btn-table">
+        <button id=${"eye-" + user.id} class="btn-edit">
+          <img src="../images/icons/eye-icon.png" alt="edit-icon">
+        </button>
+        <button id=${"edit-" + user.id} class="btn-edit">
+          <img src="../images/icons/edit-icon.png" alt="edit-icon">
+        </button>
+        <button  id=${"trash-" + user.id} class="btn-edit">
+          <img src="../images/icons/trash-icon.png" alt="edit-icon">
+        </button>
+      </td>
+    `
+
+    clientsTable.appendChild(tr)
+
+    activateClickForEye(user.id)
+    activateClickForEdit(user.id)
+    activateClickForTrash(user.id)
+  })
+}
+
+function activateClickForEye(id) {
+  const eyeButton = document.querySelector(`#eye-${id}`)
+
+  eyeButton.addEventListener('click', () => {
+    const user = users.find(user => user.id === id)
+    console.log('eye', user);
+    renderClients()
+  })
+}
+
+function activateClickForEdit(id) {
+  const eyeButton = document.querySelector(`#edit-${id}`)
+
+  eyeButton.addEventListener('click', () => {
+    const user = users.find(user => user.id === id)
+    console.log('edit', user);
+    renderClients()
+  })
+}
+
+function activateClickForTrash(id) {
+  const eyeButton = document.querySelector(`#trash-${id}`)
+
+  eyeButton.addEventListener('click', () => {
+    const user = users.find(user => user.id === id)
+    console.log('trash', user);
+    renderClients()
+  })
+}
+
+renderClients()
--- emanuel-project/src/scripts/reset.js
+++ emanuel-project/src/scripts/reset.js
@@ -1,8 +1,22 @@
 const users = [
   {
-    name: 'John Doe',
+    id: 1,
+    initials: "AB",
+    name: "Emanuel Bacalhau",
+    email: "bacalhau@gmail.com",
+    phone: "(11) 99999-9999",
+    plan: "Plano 1",
+    status: true,
+    maturity: 10
   },
   {
-    name: 'Teste',
-  }
+    id: 2,
+    initials: "CD",
+    name: "Carlos Dias",
+    email: "carlosdias@gmail.com",
+    phone: "(21) 98888-8888",
+    plan: "Plano 2",
+    status: false,
+    maturity: 8
+  },
 ]
